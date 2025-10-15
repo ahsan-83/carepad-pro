3b42b2bc10b84b2d328865c332ad24bb62e185fe
Ahsan Ali <56550635+ahsan-83@users.noreply.github.com>
2025-10-15 17:55:17 +0600

    Upload Files

--- backend/middlewares/authenticateToken.js
+++ backend/middlewares/authenticateToken.js
@@ -3,13 +3,14 @@ const jwt = require("jsonwebtoken");
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const refreshHeader = req.headers["verification"];
+  const SECRET_KEY = "4363b1c8117514b1596fd692e7eb6dee5f70e75168edc14fa02c25a00f142b1f";
   
   const token = authHeader && authHeader.split(" ")[1];
   if (!token) {
     return res.sendStatus(401);
   }
 
-  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+  jwt.verify(token, SECRET_KEY, (err, user) => {
     if (err) {
       return res.sendStatus(401);
     }
